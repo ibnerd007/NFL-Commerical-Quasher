6b7e7c44a6b2195dad67e1b32d70bf5f120ee1a9
ibnerd007 <83733064+ibnerd007@users.noreply.github.com>
2021-10-31 19:38:27 -0400

    Update commercialQuasher.py
    - Added correct region for FOX logo
    - Adjusted buffers: volume is now slow to turn down and quick to turn back up (better to turn up a commercial than to turn down the game)
    - Removed unnecessary variables

--- commercialQuasher.py
+++ commercialQuasher.py
@@ -1,11 +1,11 @@
 # NFL commerical quasher
 # Turns down volume during a commercial (when logo is not shown in upper right corner)
-# Fades volume up to full when logo reappearsq
+# Fades volume up to full when logo reappears
 
 from pyautogui import *
 import pyautogui
-import keyboard
-import win32api, win32con
+# import keyboard
+# import win32api, win32con
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -24,29 +24,33 @@ gameVolumeDb = volume.GetMasterVolumeLevel()
 maxVolume = -6.6 #dB, volume cannot rise above this level
 volDecrease = 20 # dB, level commerical volume is lowered
 
-# define regions for screens and different logos
-nbcRegion = (1435,200,100,60)
-
 def commercialQuasher(networkNum):
 	
 	networkLogos = ['nbc.png','cbs.png','fox.png','espn.png','nflnetwork.png','scoreboardonly.png']
-	networkRegions = [(1435,200,100,60),(0,0,0,0),(0,0,0,0),(0,0,0,0),(0,0,0,0),(0,0,0,0)]
+	networkRegions = [(0,0,100,100),(0,0,0,0),(1674,54,145,48),(0,0,0,0),(0,0,0,0),(0,0,0,0)]
 
 	networkLogo = networkLogos[networkNum]
 	networkRegion = networkRegions[networkNum]
 
 	print("You chose", networkLogos[networkNum])
+	print("region = ", networkRegions[networkNum])
 
 	onCommercial = False
-	falsePositives = 0
 	falseNegatives = 0
+	loToHi = 3
+	hiToLo = 10
+
+	# region=networkRegion
 
 	while 1:
-		if (pyautogui.locateOnScreen(networkLogo, region=networkRegion, confidence = 0.2) != None) & onCommercial:
-			# the game MIGHT be on, not sure yet
-			falsePositives += 1
 
-			if falsePositives >= 3: # we know the game is actually back
+		print(pyautogui.locateOnScreen(networkLogo, confidence=0.4))
+
+		if (pyautogui.locateOnScreen(networkLogo, region=networkRegion, confidence=0.3) != None) and onCommercial:
+			# the game MIGHT be on, not sure yet
+			falseNegatives += 1
+
+			if falseNegatives >= loToHi: # we know the game is actually back
 				print("The game is back!")
 
 				currentVol = volume.GetMasterVolumeLevel() # read current volume before for loop
@@ -56,15 +60,14 @@ def commercialQuasher(networkNum):
 					time.sleep(0.1)
 
 				onCommercial = False # no longer on commercial
-				falsePositives = 0 # reset false positives
+				falseNegatives = 0 # reset false positives
+		
+		elif (pyautogui.locateOnScreen(networkLogo, region=networkRegion, confidence=0.3) == None) and not onCommercial:
 
-			time.sleep(0.25)
-			
-		elif (pyautogui.locateOnScreen(networkLogo, region=networkRegion, confidence = 0.2) == None) & (onCommercial == 0):
 			# the game MIGHT be in commercial, we don't know yet
 			falseNegatives += 1
 
-			if falseNegatives >= 3: # we know the game is actually in commercial
+			if falseNegatives >= hiToLo: # we know the game is actually in commercial
 				print("The game is in commercial.")
 
 				for i in range(volDecrease + 1):
@@ -74,23 +77,22 @@ def commercialQuasher(networkNum):
 				onCommercial = True
 				falseNegatives = 0 # reset false negatives
 
-			time.sleep(0.25)
-
 		else:
-			falseNegatives = 0
-			falsePositives = 0 # neither statement qualifies; game situation maintained
-			time.sleep(0.25)
+			falseNegatives = 0 # neither statement qualifies; game situation maintained
 
 		# failsafe for loud volume
-		if volume.GetMasterVolumeLevel() >= maxVolume:
+		currVolume = volume.GetMasterVolumeLevel()
+		if currVolume >= maxVolume:
 			volume.SetMasterVolumeLevel(gameVolumeDb - volDecrease, None)
-			print("WARNING: Master volume too loud! Terminating program...")
+			print("WARNING: Master volume too loud: ({} dB)! Terminating program...".format(currVolume))
 			return
 
-		if keyboard.is_pressed('q') == True: # Press 'q' to quit
-			print("Til next time!")
-			return	
+		time.sleep(0.25)
 
-commercialQuasher(0)
+		# if keyboard.is_pressed('q') == True: # Press 'q' to quit
+		# 	print("Til next time!")
+		# 	return
+
+commercialQuasher(2)
 
 
